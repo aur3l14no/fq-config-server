c7ce26fcdf41a7dad77b9f6d368f62ee94dacc76
Aur3l14no <muzhi.yu1@gmail.com>
2020-03-03 21:45:23 +0800

    add rule, and proxy group

--- run.py
+++ run.py
@@ -4,64 +4,73 @@ import json
 import base64
 from flask import Flask, request, abort, make_response
 
-clash_config_template = '''
-port: 1081
-socks-port: 1080
-allow-lan: false
-mode: Direct
-log-level: info
-external-controller: 127.0.0.1:9090
-Rule:
-- FINAL,DIRECT
-'''
+def _filter(sub):
+    name = sub['name']
+    return '维护' not in name and \
+            '倍' not in name and \
+            ('美国' in name or '专线' in name or '[自建]' in name)
 
-
-def custom_filter(x):
-    return '维护' not in x and \
-           '倍率' not in x and \
-           ('美国' in x or '专线' in x or '[自建]' in x)
+grouped_vmess = []
 
 
 def collect():
-    '''collect from all sources and return a list of vmess urls
+    '''collect from all sources to `grouped_vmess`
     '''
+    global grouped_vmess
+    grouped_vmess = []
     with open('config.yml') as f:
         config = yaml.safe_load(f.read())
-    vmess_list = []
     if 'subscribe' in config:
-        for x in config['subscribe']:
-            r = requests.get(x)
-            vmesses = base64.b64decode(r.text).decode('ascii').splitlines()
-            vmess_list.extend(vmesses)
+        sub = config['subscribe'][0]
+        url = sub['url']
+        r = requests.get(url)
+        vmesses = base64.b64decode(r.text).decode('ascii').splitlines()
+        grouped_vmess.append({
+            'name': f'sub-{sub["name"]}',
+            'vmesses': vmesses
+        })
     if 'vmess' in config:
-        for x in config['vmess']:
-            vmess_list.append(x)
-    return vmess_list
+        grouped_vmess.append({
+            'name': 'other',
+            'vmesses': config['vmess']
+        })
 
 
 def export_clash_config():
-    vmess_list = collect()
-    proxies = []
-    for x in vmess_list:
-        j = vmess_to_clash_json(x)
-        if custom_filter(j['name']):
-            proxies.append(j)
-    names = [x['name'] for x in proxies]
-    y = yaml.safe_load(clash_config_template)
-    y['Proxy'] = proxies
+    global grouped_vmess
+
+    vmesses = []
+    for group in grouped_vmess[:-1]:
+        vmesses.extend(group['vmesses'])    
+    sub_proxies = [*filter(_filter, map(vmess_to_clash_json, vmesses))]
+
+    vmesses = grouped_vmess[-1]['vmesses']
+    other_proxies = [*filter(_filter, map(vmess_to_clash_json, vmesses))]
+    
+    yaml_scratch = ''
+    with open('Head.yml') as f:
+        yaml_scratch += f.read() + '\n'
+    with open('Rule.yml') as f:
+        yaml_scratch += f.read() + '\n'
+    y = yaml.safe_load(yaml_scratch)
+
+    y['Proxy'] = sub_proxies + other_proxies
     y['Proxy Group'] = [{
-        'name': 'auto',
+        'name': 'Proxy',
+        'type': 'select',
+        'proxies': ['sub', 'other']
+        }, {
+        'name': 'sub',
         'type': 'url-test',
         'url': 'http://www.gstatic.com/generate_204',
         'interval': 600,
-        'proxies': names
+        'proxies': [*map(lambda x: x['name'], sub_proxies)]
         }, {
-        'name': 'manual',
+        'name': 'other',
         'type': 'select',
-        'url': 'http://www.gstatic.com/generate_204',
-        'interval': 600,
-        'proxies': names
+        'proxies': [*map(lambda x: x['name'], other_proxies)]
     }]
+    y['Rule']
     return yaml.safe_dump(y, encoding='utf-8', allow_unicode=True)
 
 
@@ -120,6 +129,7 @@ def stat():
 @app.route('/clash')
 @require_auth
 def export_clash():
+    collect()
     response = make_response(export_clash_config())
     response.headers['Content-Type'] = \
         'application/octet-stream; charset=utf-8'
@@ -131,8 +141,22 @@ def export_clash():
 @app.route('/subscribe')
 @require_auth
 def export_subscribe():
-    return base64.b64encode('\n'.join(collect()))
+    collect()
+    all_vmess = []
+    for x in grouped_vmess:
+        all_vmess.extend(x['vmesses'])
+    return base64.b64encode('\n'.join(set(all_vmess)))
 
+@app.route('/clash_saved')
+@require_auth
+def get_clash_saved():
+    with open('Clash.yml') as f:
+        response = make_response(f.read())
+    response.headers['Content-Type'] = \
+        'application/octet-stream; charset=utf-8'
+    response.headers['Content-Disposition'] = \
+        'attachment; filename="config.yml"'
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
